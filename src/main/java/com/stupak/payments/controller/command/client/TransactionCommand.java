3fa66dba7b22ac07bcc9833c7bcb164e9b6d03f0
Stupak Stas <stanislav.stupakk@gmail.com>
2023-01-22 05:37:08 +0200

    1.6.9 PRG 15% done

--- src/main/java/com/stupak/payments/controller/command/client/TransactionCommand.java
+++ src/main/java/com/stupak/payments/controller/command/client/TransactionCommand.java
@@ -15,63 +15,53 @@ import javax.servlet.http.HttpSession;
 import java.util.List;
 
 public class TransactionCommand implements ICommand {
-  @Override
-  public String execute(HttpServletRequest req, HttpServletResponse resp) {
-    HttpSession session = req.getSession();
-    String forward = Path.PAGE_TRANSACTIONS;
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession();
+        String forward = Path.PAGE_TRANSACTIONS;
 
-    IAccountService as = AppContext.getInstance().getAccountService();
-    ITransactionService ts = AppContext.getInstance().getTransactionService();
-    ITransactionRowsService trs = AppContext.getInstance().getTransactionRowService();
+        IAccountService as = AppContext.getInstance().getAccountService();
+        ITransactionService ts = AppContext.getInstance().getTransactionService();
+        ITransactionRowsService trs = AppContext.getInstance().getTransactionRowService();
 
-    long id;
-    int currentPage;
-    int recordsPerPage = 6;
+        long id;
+        int currentPage;
+        int recordsPerPage = 6;
 
-    String account_id = req.getParameter("account_id");
-    if(account_id != null) {
-      id = Long.parseLong(account_id);
-    } else{
-      id = (Long)session.getAttribute("account_id");
+        String account_id = session.getAttribute("account_id").toString();
+
+        id = Long.parseLong(account_id);
+
+        String currentPageStr = req.getParameter("current_page");
+        if(currentPageStr == null) {
+            currentPageStr = session.getAttribute("current_page").toString();
+        }
+
+        currentPage = Integer.parseInt(currentPageStr);
+
+        String sorting = (String) session.getAttribute("transactions_sorting");
+
+        if (sorting == null) {
+            sorting = "new_to_old";
+        }
+
+        List<Transaction> transactions = ts.getByPage(recordsPerPage,
+                (currentPage - 1) * recordsPerPage, id, sorting);
+
+        Account account = as.getAccountById(id);
+        int rows = trs.getNumberOfRows(id);
+        int nOfPages = rows / recordsPerPage;
+        if ((rows % recordsPerPage) != 0 && nOfPages % recordsPerPage > 0) {
+
+            nOfPages++;
+        }
+
+        session.setAttribute("current_page", currentPage);
+        session.setAttribute("transactions", transactions);
+        session.setAttribute("account", account);
+        session.setAttribute("no_of_pages", nOfPages);
+        session.setAttribute("records_per_page", recordsPerPage);
+
+        return forward;
     }
-
-    String currentPageStr = req.getParameter("current_page");
-    if(currentPageStr != null) {
-      currentPage = Integer.parseInt(currentPageStr);
-    } else{
-      currentPage = (Integer)session.getAttribute("current_page");
-    }
-
-    String sorting = req.getParameter("transactions_sorting");
-
-    if (sorting == null) {
-      sorting = (String) session.getAttribute("transactions_sorting");
-      if (sorting == null) {
-        sorting = "new_to_old";
-      }
-    }
-
-    List<Transaction> transactions = ts.getByPage(recordsPerPage,
-            (currentPage - 1) * recordsPerPage, id, sorting);
-
-    Account account = as.getAccountById(id);
-    int rows = trs.getNumberOfRows(id);
-    int nOfPages = rows / recordsPerPage;
-    if ((rows % recordsPerPage) != 0 && nOfPages % recordsPerPage > 0) {
-
-      nOfPages++;
-    }
-
-    session.setAttribute("account_id", id);
-    session.setAttribute("current_page", currentPage);
-    session.setAttribute("transactions_sorting", sorting);
-    session.setAttribute("transactions", transactions);
-    session.setAttribute("account", account);
-    session.setAttribute("no_of_pages", nOfPages);
-    session.setAttribute("current_page", currentPage);
-    session.setAttribute("records_per_page", recordsPerPage);
-
-
-    return forward;
-  }
 }
